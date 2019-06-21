6322c4d418393551f283d63d4c8da8227ac9b220
Edgar Hernández <ehernand@redhat.com>
2019-06-21 16:22:38 -0500

    Something is not working OK. Re-encrypting vars and keys
    Doing key decryption in before_install as suggested by Travis CLI.

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,6 @@
 
 ## SETUP SSH CLIENT
 #
-openssl aes-256-cbc -K $encrypted_a9e5b11a096a_key -iv $encrypted_a9e5b11a096a_iv \
-  -in ./.travis/deploy-key.enc -out ./.travis/deploy-key -d
 eval $(ssh-agent -s)
 chmod 600 ./.travis/deploy-key
 echo -e "\nHost $DEPLOY_SERVER\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
